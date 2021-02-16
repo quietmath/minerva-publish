76766a0914a502ef2a56637be82004c1b44dfd80
Michael Szul <michael@szul.us>
2021-02-16 15:39:18 -0500

    Default helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,25 +6,34 @@ import { PubConfig } from './schema';
  * @module quietmath/minerva-publish
  */
 
+const range = (current: number, start: number, end: number): boolean => {
+    const adjusted: number = current + 1;
+    if((start <= adjusted) && (adjusted <= end)) {
+        return true;
+    }
+    return false;
+};
+
+const formatRSSDate = (date: string): string => {
+    if(date == null || date == '') {
+        return `${ moment().format('ddd, DD MMM YYYY hh:mm:ss') } EST`;
+    }
+    return `${ moment(date).format('ddd, DD MMM YYYY hh:mm:ss') } EST`;
+};
+
+const defaultOr = (val: string, defaultVal: string): string => {
+    if(val == null || val === '') {
+        return defaultVal;
+    }
+    return val;
+};
+
 export const registerAllPartials = (hb: any, config: PubConfig): void => {
     hb.registerPartial('layout', fs.readFileSync(`${ config.prefix }/${ config.layout }`, 'utf8'));
 };
 
 export const registerAllHelpers = (hb: any): void => {
-
-    hb.registerHelper('range', (current: number, start: number, end: number): boolean => {
-        const adjusted: number = current + 1;
-        if((start <= adjusted) && (adjusted <= end)) {
-            return true;
-        }
-        return false;
-    });
-
-    hb.registerHelper('formatRSSDate', (date: string): string => {
-        if(date == null || date == '') {
-            return `${ moment().format('ddd, DD MMM YYYY hh:mm:ss') } EST`;
-        }
-        return `${ moment(date).format('ddd, DD MMM YYYY hh:mm:ss') } EST`;
-    });
-
+    hb.registerHelper('range', range);
+    hb.registerHelper('formatRSSDate', formatRSSDate);
+    hb.registerHelper('defaultOr', defaultOr);
 };
